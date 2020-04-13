8f326cf29979b504a7e23d4587887e2b48e1d935
mcll <ymcvalu@gmail.com>
2020-04-13 20:27:57 +0800

    fix testing cases for parsing config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -9,12 +9,12 @@ import (
 func TestParseConfig(t *testing.T) {
 	cases := []struct {
 		authority string
-		succ      bool
+		valid     bool
 		cfg       *Config
 	}{
 		{
 			authority: "test:123456@127.0.0.1:2379,127.0.0.2:2379,127.0.0.3:2379?timeout=5s",
-			succ:      true,
+			valid:     true,
 			cfg: &Config{
 				Username: "test",
 				Password: "123456",
@@ -28,6 +28,7 @@ func TestParseConfig(t *testing.T) {
 		},
 		{
 			authority: "127.0.0.1:2379,127.0.0.2:2379,127.0.0.3:2379",
+			valid:     true,
 			cfg: &Config{
 				Endpoints: []string{
 					"127.0.0.1:2379",
@@ -39,11 +40,11 @@ func TestParseConfig(t *testing.T) {
 	}
 	for _, c := range cases {
 		cfg, err := Parse(c.authority)
-		if err != nil && c.succ {
+		if err != nil && c.valid {
 			t.Errorf("failed to parse %s", c.authority)
 		} else if err == nil {
 			if !reflect.DeepEqual(cfg, c.cfg) {
-				t.Errorf("has error when parsing %s %+v %+v", c.authority, *cfg, *c.cfg)
+				t.Errorf("error raised when parsing %s %+v %+v", c.authority, *cfg, *c.cfg)
 			}
 		}
 	}
